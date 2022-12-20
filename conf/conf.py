4ab6c52bc070a4444f1899423613ed9636f59a3d
Никита Сеняткин <nikitasenyatkin@MacBook-Air-Nikita-5.local>
2022-12-21 01:07:13 +0300

    final version) I beleive)

--- conf/conf.py
+++ conf/conf.py
@@ -6,5 +6,8 @@ logging.basicConfig(level=logging.INFO)
 
 current_directory = os.path.dirname(os.path.realpath(__file__))
 settings = Dynaconf(settings_file=f'{current_directory}/settings.toml')
+
+ridge = settings.MODELS.ridge
+lasso = settings.MODELS.lasso
    
 
--- entrypoint.py
+++ entrypoint.py
@@ -1,18 +1,15 @@
 from model.prediction import prediction
 from conf.conf import logging
-from conf.conf import settings
+from conf.conf import ridge, lasso
 
-
-# choose one of two models 
-ridge = settings.MODELS.ridge
-lasso = settings.MODELS.lasso
 
 # set parameters for tuning if needed. It is possible to set needed alpha for the model
 grid_params = {
     'alpha': [0, 0.5, 1, 1.5, 2, 2.5, 3]
 }
 
-# delete grid params from the argument, if it is needed to run models on default parameters 
+# delete grid params from the argument, if it is needed to run models on default parameters. 
+# use ridge or lasso for the second arg
 pred = prediction([[0, 180, 249]],lasso , grid_params)
 logging.info(f'predicted value is {pred}')
 
